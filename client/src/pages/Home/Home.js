607f1cf76705486da6debaa34d4606f591d4f845
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2019-03-09 12:44:09 -0700

    fixed regex

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import API from "../../utils/API";
 import Joke from "../../components/Joke"
