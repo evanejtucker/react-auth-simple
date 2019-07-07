7ac9bb30182d38d9dad12e5570d2814e542bdc18
Evan Tucker <eej.tucker@gmail.com>
2019-07-07 12:13:35 -0600

    updated node modules removed uneccessary import in home.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 import API from "../../utils/API";
 import Joke from "../../components/Joke"
